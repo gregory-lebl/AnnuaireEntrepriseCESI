95b4fa8beacb45142f8707e4c34f4db2f9c03454
Gregory <gregory.leblond.dev@gmail.com>
2022-03-02 15:05:01 +0100

    J'aime pas le C#

---
+++ AnnuaireEntrepriseCESI/Data/AnnuaireEntrepriseDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AnnuaireEntrepriseCESI.Data
+{
+    public class AnnuaireEntrepriseDbContext : DbContext
+    {
+
+        #region Constructor
+        public AnnuaireEntrepriseDbContext() : base()
+        {
+
+        }
+        #endregion
+
+
+        #region DbSet
+        // Créer les DbSet quand les models seront créés
+        #endregion
+
+        #region DatabaseConfiguration
+        // Définir la configuration de la base de données
+        public static string GetConnectionString()
+        {
+            return "Server=(localdb)\\MSSQLLocalDB;Database=StiveDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var _connectionString = GetConnectionString();
+
+                optionsBuilder.UseSqlServer(_connectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        #endregion
+
+        #region Seeder
+        // Insérer des fausses données
+        #endregion
+    }
+}
