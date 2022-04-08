0c64c2544b47609fe03489124fb8f97e2d83f19f
Gregory <gregory.leblond.dev@gmail.com>
2022-04-08 21:32:41 +0200

    tt

--- AnnuaireEntrepriseCESI/Views/AdminViews/AdminEmploye/UpdateOneEmploye.xaml.cs
+++ AnnuaireEntrepriseCESI/Views/AdminViews/AdminEmploye/UpdateOneEmploye.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using AnnuaireEntrepriseCESI.Data;
+using AnnuaireEntrepriseCESI.Data.StorageModels;
 using System;
 using System.Linq;
 using System.Windows;
@@ -14,18 +15,27 @@ namespace AnnuaireEntrepriseCESI.Views.AdminViews.AdminEmploye
         public UpdateOneEmploye(string lastname, string firstname, string cellPhone, string fixePhone, string email, Guid serviceId, Guid siteId)
         {
             InitializeComponent();
-            
+
+            Service service = GetEmployeService(serviceId);
 
             InputEmployeLastName.Text = lastname;
             InputEmployeFirstName.Text = firstname;
             InputEmployeCellPhoneNumber.Text = cellPhone;
             InputEmployeFixePhoneNumber.Text = fixePhone;
             InputEmployeEmail.Text = email;
-            ComboBoxService.SelectedValue = serviceId;
+            ComboBoxService.SelectedValue = service.Id;
             ComboBoxSite.SelectedValue = siteId;
 
             ComboBoxService.ItemsSource = context.Service.ToList();
             ComboBoxSite.ItemsSource = context.Site.ToList();
+        }
+        /// <summary>
+        /// Récupère le service auquel l'employé est rattaché
+        /// </summary>
+        /// <param name="id"></param>
+        private Service GetEmployeService(Guid id)
+        {
+            return context.Service.Where(o => o.Id == id).First();
         }
     }
 }
